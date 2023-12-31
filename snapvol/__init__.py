ffc5338a9da652dfadbf6e0f38b0705555f0c9f0
Emil Hammarberg <emilhammarberg@protonmail.com>
2023-12-31 21:27:57 +0100

    Add groups

--- snapvol/__init__.py
+++ snapvol/__init__.py
@@ -1,8 +1,6 @@
 import asyncio
 import snapcast.control
 import argparse
-# TODO Autocompletion? Would have to work for client names
-# TODO Print on no argument, flag for volume
 
 loop = asyncio.get_event_loop()
 server = loop.run_until_complete(snapcast.control.create_server(loop, '192.168.1.127'))
@@ -35,23 +33,32 @@ def toggle_mute(client):
 def get_volume(client):
   print(f"{client.friendly_name}: {client.volume}")
 
-# TODO Add groups as options (probably as another argument)
 clients = server.clients
 groups = server.groups
 name2client = {c.friendly_name: c for c in clients}
+identifier2client = {c.identifier: c for c in clients}
+name2group = {g.friendly_name: g for g in groups}
 
 parser = argparse.ArgumentParser(
     prog="snapvol", description="Change Snapcast clients' volume."
     )
-# TODO negative clients (all except x)
 parser.add_argument(
     "--client",
     "-c",
     nargs="+",
-    default="all",
+    default=["all"],
     help='One or more clients, or "all"',
     type=str,
-    choices=name2client.keys(),
+    choices=list(name2client.keys()).append("all"),
+    )
+parser.add_argument(
+    "--group",
+    "-g",
+    nargs="+",
+    default=["all"],
+    help='One or more groups',
+    type=str,
+    choices=list(name2group.keys()).append("all"),
     )
 parser.add_argument(
     "volume",
@@ -62,40 +69,54 @@ parser.add_argument(
     )
 parser.add_argument("--mute", action="store_true")
 parser.add_argument("--unmute", action="store_true")
-parser.add_argument("--toggle", action="store_true")
-parser.add_argument("--list", "-l", action="store_true")
-parser.add_argument("--absolute", "-a", action="store_true")
+parser.add_argument("--toggle", action="store_true", help="Toggle mute status of client(s)")
+parser.add_argument("--list", "-l", action="store_true", help="Print all clients and their volumes")
+parser.add_argument("--absolute", "-a", action="store_true", help="Set volume rather than incrementing")
 parser.add_argument(
     "--version", "-v", action="version", version="%(prog)s 0.1"
     )
-# TODO add mute positional
 args = parser.parse_args()
 
 
 def main():
-  if args.client == "all":
-    args.client = clients
-  else:
-    args.client = [name2client[name] for name in args.client]
+  if not (args.client or args.group):
+    parser.error("You must specify either a client or group")
+
+  targets = set()
+
+  if args.client:
+    if args.client == ["all"]:
+      targets = set(clients)
+    else:
+      targets.update([name2client[name] for name in args.client])
+
+  if args.group:
+    if args.group == ["all"]:
+      for g in groups:
+        targets.update([identifier2client[identifier] for identifier in g.clients])
+    else:
+      targets.update([name2group[name] for name in args.group])
 
   if args.mute:
-    for c in args.client:
+    for c in targets:
       mute(c)
   elif args.unmute:
-    for c in args.client:
+    for c in targets:
       unmute(c)
   elif args.toggle:
-    for c in args.client:
+    for c in targets:
       toggle_mute(c)
 
   elif args.list:
-    for c in args.client:
+    for c in targets:
       get_volume(c)
 
   else:
-    for c in args.client:
+    for c in targets:
       if args.absolute:
         set_volume(c, args.volume)
       else:
         change_volume(c, args.volume)
 
+if __name__ == "__main__":
+  main()
